7cb1469e1dd5c24bc11a100de15e969cc92a6f38
DarrenGameDev <166923450+DarrenGohGameDev@users.noreply.github.com>
2025-06-10 22:02:51 +0800

    changes to attribute component * prob be remove in the future
    changes to attribute component * prob be remove in the future

---
+++ Source/ThirdPersonTemplate/Public/Character/AttributeComponent.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+//#include "AttributeSet.h"
+#include "Components/ActorComponent.h"
+#include "AttributeComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class THIRDPERSONTEMPLATE_API UAttributeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UAttributeComponent();
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	/*UPROPERTY(EditAnywhere, Category = "Actor Attributes")
+	float CurrentHealth = 100.f;*/
+
+	/*UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes")
+	FGameplayAttributeData CurrentHealth;*/
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:	
+	
+
+		
+};
