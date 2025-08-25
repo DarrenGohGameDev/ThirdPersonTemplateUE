1ad3efb7d0a8903b98cbf5bcaf5e4623e416f60c
DarrenGameDev <166923450+DarrenGohGameDev@users.noreply.github.com>
2025-08-25 17:47:27 +0800

    Added interactable interface
    -  Added interactable interface
    - base item interact now overrides Interactable interface

--- Plugins/ThirdPersonGamePlugin/Source/ThirdPersonGamePlugin/Public/Item/BaseItemClass.h
+++ Plugins/ThirdPersonGamePlugin/Source/ThirdPersonGamePlugin/Public/Item/BaseItemClass.h
@@ -4,13 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Interface/Interactable.h"
 #include "BaseItemClass.generated.h"
 
 class UItemAttributeSet;
 class UAbilitySystemComponent;
 
 UCLASS()
-class THIRDPERSONGAMEPLUGIN_API ABaseItemClass : public AActor
+class THIRDPERSONGAMEPLUGIN_API ABaseItemClass : public AActor , public IInteractable
 {
 	GENERATED_BODY()
 	
@@ -21,7 +22,7 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	virtual void Interact();
+	virtual void Interact() override;
 
 	virtual void UseItem();
 
